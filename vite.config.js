c4b9530b63cbc0244d38ffd1d530de9f1cdb4e9d
Duncan Maina <duncandevs@gmail.com>
2024-11-25 17:17:05 -0600

    rename styles

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@styles': path.resolve(__dirname, './src/styles'),
-      // You can add more aliases here, like:
-      // '@components': path.resolve(__dirname, './src/components'),
-      // '@utils': path.resolve(__dirname, './src/utils'),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     '@styles': path.resolve(__dirname, './src/styles'),
+  //     // You can add more aliases here, like:
+  //     // '@components': path.resolve(__dirname, './src/components'),
+  //     // '@utils': path.resolve(__dirname, './src/utils'),
+  //   },
+  // },
 })
