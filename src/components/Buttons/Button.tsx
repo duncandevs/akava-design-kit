7ba884df4c600ea56783c2c2579c6be8b7368043
Duncan Maina <duncandevs@gmail.com>
2024-11-27 17:30:57 -0600

    update Button props types

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,58 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Button.css';
-import {cn} from '../../utils';
-
 /**
  * Primary UI Button component
  * Three Variants: Primary, Outline, Clear
  */
-const Button = ({ label, size, variant, disabled, LeftIcon, RightIcon, ...props }) => {
-  const btnClassNames = cn(variant, size);
-  const labelClassNames = cn(size, disabled && 'disabled');
-  const iconClassNames = cn('icon', size, disabled && 'disabled');
+import React from 'react';
+import './Button.css';
+import {cn} from '../../utils';
+import { Type as LucideIcon } from 'lucide-react'
 
-  return <button className={btnClassNames} {...props} disabled={disabled}>
-      { LeftIcon && <LeftIcon className={iconClassNames} /> }
-      <label className={labelClassNames}>{label}</label>
-      { RightIcon && <RightIcon className={iconClassNames} /> }
-    </button>;
-};
 
-Button.propTypes = {
-   /**
-   * Button display text
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Onclick handler for the button
-   */
-  onClick: PropTypes.func.isRequired,
-  /**
-   * Vairant determines button style
-   */
-  variant: PropTypes.string,
-  /**
-  * Size determines button size
-  */
-  size: PropTypes.string,
-  /**
-  * Sets the buttons disabled state
-  */
-  disabled: PropTypes.bool,
-   /**
-  * LeftIcon renders diplays an icon to the left
-  */
-  LeftIcon: PropTypes.element,
-  /**
-  * RightIcon renders diplays an icon to the right
-  */
-  RightIcon: PropTypes.element
-};
+type ButtonVariants = 'primary' | 'outline' | 'clear';
+type ButtonProps = React.ComponentProps<"button"> & {
+  label: string;
+  size: string;
+  variant: ButtonVariants;
+  LeftIcon: typeof LucideIcon;
+  RightIcon: typeof LucideIcon;
+}
 
-Button.defaultProps = {
-  variant: 'primary',
-  size: 'medium'
-};
+const Button = React.forwardRef<HTMLInputElement, ButtonProps>(
+  ({ label, size, variant, disabled, LeftIcon, RightIcon, ...props }) => {
+    const btnClassNames = cn(variant, size);
+    const labelClassNames = cn(size, disabled && 'disabled');
+    const iconClassNames = cn('icon', size, disabled && 'disabled');
+
+    return <button className={btnClassNames} {...props} disabled={disabled}>
+        { LeftIcon && <LeftIcon className={iconClassNames} /> }
+        <label className={labelClassNames}>{label}</label>
+        { RightIcon && <RightIcon className={iconClassNames} /> }
+      </button>;
+  }
+);
 
 export default Button;
